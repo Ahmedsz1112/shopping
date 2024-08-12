33738d760e8051b0bb32482ea2d0cc3e46979fcc
Ahmed Salah <ahmedsz599@gmail.com>
2024-08-13 01:53:45 +0300

    Done page login by html and css and js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
+let emailEl = document.getElementById("floatingInput");
+let passwordEl = document.getElementById("floatingPassword");
+let passShow = document.getElementById("passShow");
+let form = document.getElementById("form")
+
+
+window.onload = () => {
+    if((localStorage.getItem("email") != null) && localStorage.getItem("pass") != null){
+        emailEl.value = JSON.parse(localStorage.getItem("email"));
+        passwordEl.value = JSON.parse(localStorage.getItem("pass"));
+        login()
+}
+}
+
+function login(){
+    let user = {
+        email: emailEl.value,
+        password: passwordEl.value
+    }
+    localStorage.setItem("email" , JSON.stringify(user.email));
+    localStorage.setItem("pass" , JSON.stringify(user.password));
+}
+
+passShow.addEventListener("click" , () => {
+    if(passwordEl.type === "password") {
+        passwordEl.type = "text";
+        passShow.setAttribute( "class" ,"fa-regular fa-eye-slash");
+    }else {
+        passwordEl.type = "password";
+        passShow.setAttribute( "class" ,"fa-regular fa-eye");
+    }
+})
+
+
 
