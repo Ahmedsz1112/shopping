78431354eacfc6bd581a4b2741f0b36a578aa9a2
Ahmed Salah <ahmedsz599@gmail.com>
2024-08-13 14:28:20 +0300

    Test

--- star-shop/script.js
+++ star-shop/script.js
@@ -1,1 +1,10 @@
+let logOut = document.getElementById("logOut");
+let show = document.getElementById("show");
+
+logOut.addEventListener("click" , () => {
+    event.preventDefault()
+    show.style.display = "block";
+});
+
+
 
