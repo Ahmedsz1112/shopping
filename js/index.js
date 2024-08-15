958ab02955382df846491cea7a0e8fdf4ab2082e
Ahmed Salah <ahmedsz599@gmail.com>
2024-08-15 15:32:58 +0300

    end the project

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 let emailEl = document.getElementById("floatingInput");
 let passwordEl = document.getElementById("floatingPassword");
 let passShow = document.getElementById("passShow");
-let form = document.getElementById("form")
-
 
 window.onload = () => {
     if((localStorage.getItem("email") != null) && localStorage.getItem("pass") != null){
         emailEl.value = JSON.parse(localStorage.getItem("email"));
         passwordEl.value = JSON.parse(localStorage.getItem("pass"));
         login()
-}
+    } 
 }
 
 function login(){
@@ -24,12 +22,9 @@ function login(){
 passShow.addEventListener("click" , () => {
     if(passwordEl.type === "password") {
         passwordEl.type = "text";
-        passShow.setAttribute( "class" ,"fa-regular fa-eye-slash");
+        passShow.setAttribute( "class" ,"fa-regular fa-eye");
     }else {
         passwordEl.type = "password";
-        passShow.setAttribute( "class" ,"fa-regular fa-eye");
+        passShow.setAttribute( "class" ,"fa-regular fa-eye-slash");
     }
 })
-
-
-
